7e424b5064acf8d8ea2296435fbfaa1f61b04a39
kunalvirdi <kunalvirdi.patiala@gmail.com>
2024-10-21 14:31:52 +0530

    added constants for all devices

---
+++ src/main/java/smarthomeautomation/Constants.java
@@ -0,0 +1,13 @@
+package smarthomeautomation;
+
+public class Constants{
+    public enum SmartDevices{
+        LIGHTBULB, CAMERA
+    }
+    public enum BulbModels{
+        LUMINABASIC, LUMINACOLOR, LUMINAPRO;
+    }
+    public enum CameraModel{
+        GUARDBASIC, GUARDELITE, GUARDPRO
+    }
+}
